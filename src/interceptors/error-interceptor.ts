7243311212680be0a4b0ba2bc5d6ccc30e3e15fe
André Gustavo Bastos <agbastos@hotmail.com>
2021-06-03 02:27:08 -0300

    Tratando erros 403

--- src/interceptors/error-interceptor.ts
+++ src/interceptors/error-interceptor.ts
@@ -1,9 +1,13 @@
 import { Injectable} from '@angular/core';
 import { HttpEvent, HttpInterceptor, HttpHandler, HttpRequest, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { Observable } from 'rxjs/Rx';
+import { StorageService } from '../services/storage.service';
 
 @Injectable()
 export class ErroInterceptor implements HttpInterceptor {
+
+  constructor(public storage: StorageService) {
+  }
 
   intercept(req: HttpRequest<any>,next: HttpHandler): Observable<HttpEvent<any>> {
     return next.handle(req)
@@ -19,8 +23,18 @@ export class ErroInterceptor implements HttpInterceptor {
         console.log('Erro detectado pelo interceptor:');
         console.log(errorObj);
 
+        switch(errorObj.status) {
+          case 403:
+            this.handle403();
+            break;
+        }
+
       return Observable.throw(errorObj);
     }) as any;
+  }
+
+  handle403() {
+    this.storage.setLocaluser(null);
   }
 }
 
