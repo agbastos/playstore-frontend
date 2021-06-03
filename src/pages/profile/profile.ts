060559f15dec8c078eed8d043434891c7bab3196
André Gustavo Bastos <agbastos@hotmail.com>
2021-06-03 01:58:49 -0300

    Mostrando dados e imagens do cliente para a página de profile

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { API_CONFIG } from '../../config/api.config';
+import { ClienteDTO } from '../../models/cliente.dto';
+import { ClienteService } from '../../services/domain/cliente.service';
 import { StorageService } from '../../services/storage.service';
 
 @IonicPage()
@@ -9,19 +12,36 @@ import { StorageService } from '../../services/storage.service';
 })
 export class ProfilePage {
 
-  email: string;
+  clienteDTO: ClienteDTO;
 
   constructor(
     public navCtrl: NavController, 
     public navParams: NavParams,
-    public storage: StorageService) {
+    public storage: StorageService,
+    public clienteService: ClienteService) {
   }
 
   ionViewDidLoad() {
     let localUser = this.storage.getLocaluser();
     if (localUser && localUser.email) {
-      this.email = localUser.email;
+      this.clienteService.findByEmail(localUser.email)
+      .subscribe(
+        response => {
+          this.clienteDTO = response;
+          this.getImageIfExists();
+        },
+        error => {});
     }
+  }
+
+  getImageIfExists() {
+    this.clienteService.getImageFromBucket(this.clienteDTO.id)
+    .subscribe(
+      response => {
+        this.clienteDTO.imageUrl = `${API_CONFIG.bucketBaseUrl}/imagens/cp${this.clienteDTO.id}.jpg`;
+      },
+      error => {}
+    );
   }
 
 }
